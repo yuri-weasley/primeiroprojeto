126c5991176ff99458c9e7d1e262ed5e1032f46d
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-12-15 15:50:55 -0400

    Primeiros códigos

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    /*  ANOTAÇÕES
+        O JDK (Java Development Kit) é um conjunto de ferramentas e bibliotecas de software que permite o desenvolvimento de aplicativos Java.
+
+        O Java é uma plataforma de programação poderosa e versátil, com quase 30 anos de história e evolução.
+        Artigo: https://www.alura.com.br/artigos/java
+
+        Existem diversas funcionalidades que podemos utilizar no IntelliJ IDEA no cotidiano que aumentam a produtividade no trabalho em projetos Java.
+        Artigo: https://www.alura.com.br/artigos/intellij-idea-dicas-truques-usar-no-dia-a-dia
+
+        A JVM é uma ferramenta muito importante no mundo Java, pois ela permite que uma aplicação possa ser executada em múltiplos sistemas operacionais distintos.
+        Artigo: https://www.alura.com.br/artigos/jvm-conhecendo-processo-execucao-de-codigo
+
+    */
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.printf("Concluí a aula 01 e agora estou mergulhando em Java!");
     }
 }
